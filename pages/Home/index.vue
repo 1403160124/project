8b1320e89bce5e0fa43eb452d33b573f6652af29
Marfilion <43163434+1403160124@users.noreply.github.com>
2022-05-26 16:35:18 +0800

    Add files via upload

---
+++ pages/Home/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- 全局的三级菜单 -->
+    <typeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor v-for="(floor,index) in floorList" :key="floor.id" :list="floor"/>
+    <Brand />
+  </div>
+</template>
+
+<script>
+import ListContainer from "./ListContainer";
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Brand from "./Brand";
+import Floor from "./Floor";
+import { mapState } from "vuex";
+export default {
+  name: "",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Brand,
+    Floor,
+  },
+  mounted() {
+    //获取floor组件的数据
+    this.$store.dispatch("getFloorList");
+    this.$store.dispatch('getUserInfo');
+  },
+  computed: {
+    ...mapState({
+      floorList: (state) => state.home.floorList,
+    }),
+  },
+};
+</script>
+
+<style scoped></style>
